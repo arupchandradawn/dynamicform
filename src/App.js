44978859e50f8036fc23393cf6329d58e2cc3203
arupchandradawn <arup.chandra@aspiresys.com>
2022-10-20 18:25:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from "react";
 
 function App() {
+  
+  const [formFields, setFormFields ] = useState([
+    { name: '', age: '' , email :''},
+    { name: '', age: '' , email :''},
+    { name: '', age: '' , email :''}
+  ]); 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {formFields.map((form)=>{
+        return(
+          <div>
+            <input name="name" placeholder='Name'></input>
+            <input name="age" placeholder='Age'></input>
+            <input name="email" placeholder='email'></input>
+          </div>
+        )
+      })}
+      
+      <button>Add more</button>
+      <br/>
+      <button>Submit</button>
     </div>
   );
 }
